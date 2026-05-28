f2ef85920682d2400426200d495cb7533c880c70
Sanjusree <sanjusree.bommaluleni2007@gmail.com>
2026-05-28 23:26:48 +0530

    Initial commit: MySQL connection and Express setup

---
+++ db (1).js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Sush@1234',
+    database: 'sportsdb'
+});
+
+db.connect((err) => {
+
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("Connected to MySQL");
+    }
+
+});
+
+module.exports = db;
---
+++ server (1).js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const app = express();
+
+const db = require('./db');
+
+
+// ROUTE
+app.get('/athletes', (req, res) => {
+
+    const sql = "SELECT * FROM athletes";
+
+    db.query(sql, (err, result) => {
+
+        if(err){
+            console.log(err);
+            res.send(err);
+        }
+        else{
+            res.send(result);
+        }
+
+    });
+
+});
+
+
+// SERVER
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
+updated
